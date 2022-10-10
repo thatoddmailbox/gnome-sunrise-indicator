f71aa1d2001903f790ea4cf449caa85d79709400
Alex Studer <alex@studer.dev>
2022-10-10 17:30:36 -0400

    can request times from api and update icon + text

--- extension.js
+++ extension.js
@@ -1,9 +1,35 @@
-
 const St = imports.gi.St;
 const Main = imports.ui.main;
 const Tweener = imports.ui.tweener;
+const Soup = imports.gi.Soup;
+
+// TODO: make this configurable somehow?
+const currentLocation = {
+    latitude: "42.378385",
+    longitude: "-71.147929"
+};
+const iconSunset = "daytime-sunset-symbolic";
+const iconSunrise = "daytime-sunrise-symbolic";
+
+const sessionSync = new Soup.SessionSync();
 
 let text, button;
+let statusIcon;
+let statusLabel;
+
+function updateData() {
+    const msg = Soup.Message.new("GET", "https://api.sunrise-sunset.org/json?lat=" + currentLocation.latitude + "&lng=" + currentLocation.longitude + "&formatted=0");
+    sessionSync.queue_message(msg, (session, msg) => {
+        const apiData = JSON.parse(msg.response_body.data);
+        const sunset = new Date(apiData.results.sunset);
+        const sunrise = new Date(apiData.results.sunrise);
+        log("msg", msg.response_body.data);
+        log("sunset", sunset);
+        log("sunrise", sunrise);
+        statusLabel.set_text(sunset.toLocaleTimeString());
+        statusIcon.set_icon_name(iconSunset);
+    });
+}
 
 function _hideHello() {
     Main.uiGroup.remove_actor(text);
@@ -11,6 +37,9 @@ function _hideHello() {
 }
 
 function _showHello() {
+    log("_showHello");
+    updateData();
+
     if (!text) {
         text = new St.Label({ style_class: 'helloworld-label', text: "Hello, world!" });
         Main.uiGroup.add_actor(text);
@@ -31,17 +60,29 @@ function _showHello() {
 }
 
 function init() {
-    button = new St.Bin({ style_class: 'panel-button',
-                          reactive: true,
-                          can_focus: true,
-                          x_fill: true,
-                          y_fill: false,
-                          track_hover: true });
-    let icon = new St.Icon({ icon_name: 'daytime-sunrise-symbolic',
-                             style_class: 'system-status-icon' });
+    button = new St.Bin({
+        style_class: 'panel-button',
+        reactive: true,
+        can_focus: true,
+        x_fill: true,
+        y_fill: false,
+        track_hover: true
+    });
+    let buttonContainer = new St.BoxLayout();
 
-    button.set_child(icon);
-    button.connect('button-press-event', _showHello);
+    statusIcon = new St.Icon({
+        icon_name: iconSunrise,
+        style_class: "system-status-icon"
+    });
+    buttonContainer.add_child(statusIcon);
+
+    statusLabel = new St.Label({
+        text: "Hello, world!"
+    });
+    buttonContainer.add_child(statusLabel);
+
+    button.set_child(buttonContainer);
+    button.connect("button-press-event", _showHello);
 }
 
 function enable() {
